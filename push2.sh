a93b06dbe207ecf7673ba13b31ba8092f978027c
alex <alex@qq.com>
2021-05-30 01:35:44 +0800

    new update, 5284

--- push2.sh
+++ push2.sh
@@ -5,7 +5,7 @@ count=5000
 for ((i=0; i<${count}; i++))
 do
 randNumer=$(($RANDOM%500000000))
-echo $randNumer >> ./README.md
+echo $randNumer >> ./index.c
 git add .
 git commit -m "new update, ${randNumer}"
 done
